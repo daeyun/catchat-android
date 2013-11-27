c895efc319b49a6d9102b1945d2ad1240e4dfc62
Daeyun Shin <daeyunshin@gmail.com>
2013-11-27 04:13:04 -0600

    added layoutNotNull test

--- CatChat/src/test/java/com/daeyunshin/catchat/android/tests/MainActivityTest.java
+++ CatChat/src/test/java/com/daeyunshin/catchat/android/tests/MainActivityTest.java
@@ -1,15 +1,40 @@
 package com.daeyunshin.catchat.android.tests;
 
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.daeyunshin.catchat.android.MainActivity;
+import com.daeyunshin.catchat.android.R;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
 
-import static org.junit.Assert.assertTrue;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 @RunWith(RobolectricGradleTestRunner.class)
 public class MainActivityTest {
 
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.buildActivity(MainActivity.class).create().resume().get();
+    }
+
     @Test
-    public void shouldFail() {
-        assertTrue(false);
+    public void testLayoutNotNull() {
+        assertThat(activity).isNotNull();
+
+        ListView listView = (ListView) activity.findViewById(R.id.list);
+        assertThat(listView).isNotNull();
+
+        Button sendButton = (Button) activity.findViewById(R.id.button_send);
+        assertThat(sendButton).isNotNull();
+
+        EditText editMessage = (EditText) activity.findViewById(R.id.edit_message);
+        assertThat(editMessage).isNotNull();
     }
 }
