df4d8e9be702b3bd424b67da2c1b123b3996585a
Daeyun Shin <daeyunshin@gmail.com>
2013-11-27 03:14:50 -0600

    added unit testing libraries and tests folder

---
+++ CatChat/src/test/java/com/daeyunshin/catchat/android/tests/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.daeyunshin.catchat.android.tests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+
+    @Test
+    public void shouldFail() {
+        assertTrue(false);
+    }
+}
